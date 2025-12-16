9e4e388ca6b68599131c569b30abe77014434baf
davidramosoler <davidramosoler@gmail.com>
2025-12-16 19:57:33 +0000

    refactor: Restructure project for Vercel deployment

--- api/server.js
+++ api/server.js
@@ -12,10 +12,6 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir archivos estáticos para las imágenes
-app.use('/IMG', express.static(path.join(__dirname, '..', 'frontend', 'IMG')));
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
-
 // Rutas de la API
 const authRoutes = require('./routes/authRoutes');
 const contentRoutes = require('./routes/contentRoutes');
